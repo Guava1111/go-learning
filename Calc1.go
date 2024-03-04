f2e34b5f8645a04199cf7807612dd3b385861423
Guava1111 <161199328+Guava1111@users.noreply.github.com>
2024-03-04 20:43:54 +0800

    new-Calc1.go
    計算機 修改

--- Calc1.go
+++ Calc1.go
@@ -1,29 +1,40 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "strings"
+    "strconv"
+    
+)
 
 func main(){
-var  x, y int
-var result int
-var a string
+    var x string
+    var reult int
+    fmt.Scanln(&x)
+   
+    //parts := strings.Split(x,"+") 找到字串裡的+剃除後印出
+    parts := strings.Split(x,"")//一個一個分開印出
+     num1,_ := strconv.Atoi(parts[0])
+     num2,_ := strconv.Atoi(parts[2])
+    fmt.Println(num1)
+    fmt.Println(num2)
+    if parts[1] == "+"{
+        
+        reult = num1 + num2
+        fmt.Println(reult)
+       }
+    if parts[1] == "-"{
+        reult = num1 - num2
+        fmt.Println(reult)
+       }
+    if parts[1] == "/"{
+        reult = num1 / num2
+        fmt.Println(reult)
+    }
+    if parts[1] == "*"{
+        reult = num1*num2
+        fmt.Println(reult)
+    }
 
-fmt.Printf("請輸入:")
-fmt.Scan(&x)
-fmt.Printf("請輸入運算符號:")
-fmt.Scan(&a)
-fmt.Printf("請輸入:")
-fmt.Scan(&y)
-//fmt.Println(x, a, y)
-//result = fmt.Sprintln(x,a,y)
-if a=="+"{
-	result = x+y
-	fmt.Println("答案:",result)
+
 }
-if a == "-"{
-	result = x-y
-	fmt.Println("答案:",result)
-}
-if a == "/"{
-	result = x/y
-	fmt.Println("答案:",result)
-}
-}
+
